4be7be004f79b862ec6218af7b18d822a7a02e61
Sourcery AI <>
2022-08-26 06:01:11 +0000

    'Refactored by Sourcery'

--- flights/views.py
+++ flights/views.py
@@ -64,11 +64,10 @@ def link_callback(uri, rel):
     Convert HTML URIs to absolute system paths so xhtml2pdf can access those
     resources
     """
-    result = finders.find(uri)
-    if result:
+    if result := finders.find(uri):
         if not isinstance(result, (list, tuple)):
             result = [result]
-        result = list(os.path.realpath(path) for path in result)
+        result = [os.path.realpath(path) for path in result]
         path = result[0]
     else:
         sUrl = settings.STATIC_URL        # Typically /static/
@@ -85,9 +84,7 @@ def link_callback(uri, rel):
 
     # make sure that file exists
     if not os.path.isfile(path):
-        raise Exception(
-            'media URI must start with %s or %s' % (sUrl, mUrl)
-        )
+        raise Exception(f'media URI must start with {sUrl} or {mUrl}')
     return path
 
 
@@ -95,11 +92,11 @@ def render_pdf_view(request):
     flights = Flight.objects.all()
     template_path = 'test.html'
     context = {'myvar': 'this is your template context', 'flights': flights}
-   
+
     # Create a Django response object, and specify content_type as pdf
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="report.pdf"'
-    
+
     # find the template and render it.
     template = get_template(template_path)
     html = template.render(context)
@@ -109,7 +106,7 @@ def render_pdf_view(request):
         html, dest=response, link_callback=link_callback)
     # if error then show some funy view
     if pisa_status.err:
-        return HttpResponse('We had some errors <pre>' + html + '</pre>')
+        return HttpResponse(f'We had some errors <pre>{html}</pre>')
     return response
 
 #Generate PDF END
